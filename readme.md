93ba147e5988644a0a1ba0ca603e1822c176011d
komiyamma <komiyama0629@gmail.com>
2024-09-27 05:45:30 +0900

    こみっと

---
+++ readme.md
@@ -0,0 +1,5 @@
+# AMD Software Adrenalin Edition を自動的に閉じる
+
+「AMD Software Adrenalin Edition」がWindowsのロックを解除するタイミングで、なぜかウィンドウを広げてしまうので、  
+その際に自動的にウィンドウを閉じるようにするためのツール。
+
---
+++ src/AMDSoftwareAdrenalinEditionAutoCloser/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadeonSoftwareTerm
+{
+    internal class Program
+    {
+
+
+        static async Task Main(string[] args)
+        {
+            var count = 0;
+
+            for(int i=0; i<8; i++)
+            {
+                if (count == 0)
+                {
+                    await Task.Delay(100); // 0.1秒待つ
+                    count = KillTargetProcess();
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private static int KillTargetProcess()
+        {
+            // RadeonSoftware.exe というプログラムがあったら、それを終了する
+            var processes = System.Diagnostics.Process.GetProcessesByName("RadeonSoftware");
+
+            if (processes.Length > 0)
+            {
+                foreach (var process in processes)
+                {
+                    process.Kill();
+                }
+            }
+
+            return processes.Length;
+        }
+
+        private static void OnSessionEnding(object sender, SessionEndingEventArgs e)
+        {
+            // ログアウトまたはシャットダウンの処理
+            if (e.Reason == SessionEndReasons.Logoff)
+            {
+
+            }
+            else if (e.Reason == SessionEndReasons.SystemShutdown)
+            {
+            }
+        }
+    }
+}
